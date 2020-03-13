93e3ba2a4b2193e598df1e9e8cf62772c24bd7ee
aal-brown <adlai.brown@gmail.com>
2020-03-13 23:41:05 +0000

    Updated index.js to work with Netlify.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 
 import "index.scss";
 
 import Application from "components/Application";
 
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
+
 //Inserts the application component after "root"
 ReactDOM.render(<Application />, document.getElementById("root"));
+
