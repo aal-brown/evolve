f198e9974c1154d59bc92b57f865bb8c799f0875
aal-brown <adlai.brown@gmail.com>
2020-03-14 21:05:01 +0000

    Adding skeleton code to create the component.

--- src/components/PageContent/MyGames.js
+++ src/components/PageContent/MyGames.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { LoremIpsum } from "lorem-ipsum";
+import MyGamesItem from "MyGamesItem"
+/* import { LoremIpsum } from "lorem-ipsum";
 
 const lorem = new LoremIpsum({
   sentencesPerParagraph: {
@@ -10,12 +11,25 @@ const lorem = new LoremIpsum({
     max: 16,
     min: 4
   }
-});
+}); */
+
+// Games will be received from the database, sorted by user id.
 
 export default function MyGames() {
-  return (
-    <section>
-      <p><h1>MY GAMES</h1></p>
-    </section>
-  );
+  let games = props.games.map((game) => {
+    return (
+      <MyGamesItem
+        key={game.id}
+        created_at={game.created_at}
+        updated_at={game.updated_at}
+        age={game.age}
+        name={game.name}
+        cells={game.cells}
+        selected={game.name === props.game}
+        play={props.play} //This is a function to start the game
+        delete={props.delete} //This will prompt for whether the user wants to delete that game
+        />
+    );
+  });
+  return games;
 }
