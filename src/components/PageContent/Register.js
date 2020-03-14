d0d5d5c312f952dfa6e3dcaf075a8e6f5c92bf00
NoahDumba <noahdumba1@gmail.com>
2020-03-14 16:57:20 -0600

    console logs input values on submit. styling is gross

--- src/components/PageContent/Register.js
+++ src/components/PageContent/Register.js
@@ -1,21 +1,53 @@
-import React from "react";
-import { LoremIpsum } from "lorem-ipsum";
+import React, { useState } from "react";
+import "components/PageContent/Login.scss";
+import Button from "../Button"
 
-const lorem = new LoremIpsum({
-  sentencesPerParagraph: {
-    max: 8,
-    min: 4
-  },
-  wordsPerSentence: {
-    max: 16,
-    min: 4
+export default function Login() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+
+  function register() {
+    console.log("AGAHAGAHGA: ", name, email, username, password, passwordConfirm)
   }
-});
-
-export default function Register() {
   return (
-    <section>
-      <p><h1>REGISTER</h1></p>
+    <section className="login">
+      <h1>Register</h1>
+      <form className="login-form" onSubmit={(event) => {event.preventDefault()}}>
+        Name: <input
+          onChange={(event) => {
+            setName(event.target.value)}}
+          value={name}
+          type="text"
+        />
+        Email: <input
+          onChange={(event) => {
+            setEmail(event.target.value)}}
+          value={email}
+          type="email"
+        />
+        Username: <input
+          onChange={(event) => {
+            setUsername(event.target.value)}}
+          value={username}
+          type="text"
+        />
+        Password: <input
+          onChange={(event) => {
+            setPassword(event.target.value)}}
+          value={password}
+          type="password"
+        />
+        Confirm Password: <input
+          onChange={(event) => {
+            setPasswordConfirm(event.target.value)}}
+          value={passwordConfirm}
+          type="password"
+        />
+      </form>
+      <Button className="button--confirm" onClick={register}>Register</Button>
     </section>
   );
 }
