a80f1dffeac2c1bb459efd428232e07e088bbde5
aal-brown <adlai.brown@gmail.com>
2020-03-14 00:24:36 +0000

    Fixed syntax issue with import.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import axios from "axios";
 import "index.scss";
+import Application from "components/Application";
 
 const axios = require("axios").default
 
-
-
-import Application from "components/Application";
 
 if (process.env.REACT_APP_API_BASE_URL) {
   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
