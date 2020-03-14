4b817545e188203a844e41442055fd818f118d74
NoahDumba <noahdumba1@gmail.com>
2020-03-14 16:26:07 -0600

    login page console logs email and password now. Styling is still disgusting

--- src/components/PageContent/Login.js
+++ src/components/PageContent/Login.js
@@ -1,20 +1,34 @@
-import React from "react";
-
-const lorem = new LoremIpsum({
-  sentencesPerParagraph: {
-    max: 8,
-    min: 4
-  },
-  wordsPerSentence: {
-    max: 16,
-    min: 4
-  }
-});
+import React, { useState } from "react";
+import "components/PageContent/Login.scss";
+import Button from "../Button"
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function login() {
+    console.log("AGAHAGAHGA: ", email, password)
+  }
   return (
-    <section>
-      <p><h1>LOGIN</h1></p>
+    <section className="login">
+      <h1>Login</h1>
+      <form className="login-form" onSubmit={(event) => {event.preventDefault()}}>
+        Email: <input
+          onChange={(event) => {
+            setEmail(event.target.value)}}
+          value={email}
+          type="email"
+          placeholder="Enter Email"
+        />
+        Password: <input
+          onChange={(event) => {
+            setPassword(event.target.value)}}
+          value={password}
+          label="Password: "
+          type="password"
+        />
+      </form>
+      <Button className="button--confirm" onClick={login}>Login</Button>
     </section>
   );
 }
