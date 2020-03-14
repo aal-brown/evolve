1d0947f31d547400c8eae83b47c597e745694c96
aal-brown <adlai.brown@gmail.com>
2020-03-14 00:20:31 +0000

    Test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import axios from "axios";
+// import axios from "axios";
+
+const axios = require("axios").default
 
 import "index.scss";
 
