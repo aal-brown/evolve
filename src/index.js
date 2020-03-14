7e3061a09c94a0198ac45f420aae28e525561e0c
aal-brown <adlai.brown@gmail.com>
2020-03-14 00:28:20 +0000

    Testing the .env file to fix the bug.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import axios from "axios";
+import axios from "axios";
 import "index.scss";
 import Application from "components/Application";
 
-const axios = require("axios").default
 
 
 if (process.env.REACT_APP_API_BASE_URL) {
