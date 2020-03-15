ea57c39721515a7fc7914bd4de650f295760e5f0
Lucy <lucymidgley13@gmail.com>
2020-03-15 17:34:47 +0000

    created container for game

--- src/components/PageContent/Game.js
+++ src/components/PageContent/Game.js
@@ -1,21 +1,50 @@
-import React from "react";
-import { LoremIpsum } from "lorem-ipsum";
-
-const lorem = new LoremIpsum({
-  sentencesPerParagraph: {
-    max: 8,
-    min: 4
-  },
-  wordsPerSentence: {
-    max: 16,
-    min: 4
+import React, {
+  Component
+} from 'react'
+import Phaser from 'phaser'
+import {
+  IonPhaser
+} from '@ion-phaser/react'
+import Preload from "./scenes/preload";
+import TitleScene from './scenes/TitleScene';
+class Game extends Component {
+  state = {
+    initialize: true,
+    game: {
+      width: 800,
+      height: 600,
+      parent: 'game-container',
+      type: Phaser.AUTO,
+      physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scale: {
+      mode: Phaser.Scale.FIT,
+    },
+    scene: [Preload, TitleScene]
+    }
   }
-});
 
-export default function Game() {
-  return (
-    <section>
-      <p><h1>GAME</h1></p>
-    </section>
-  );
+  render() {
+    const {
+      initialize,
+      game
+    } = this.state
+    return ( 
+      <div id="game-container">
+      <IonPhaser game = {
+        game
+      }
+      initialize = {
+        initialize
+      }
+      />
+      </div>
+    )
+  }
 }
+
+export default Game;
