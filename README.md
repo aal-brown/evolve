2c3d7da5014df017902b9c78d146f492258e52b5
Lucy <lucymidgley13@gmail.com>
2020-03-25 20:17:37 -0600

    added content to readme and uploaded images

--- README.md
+++ README.md
@@ -1,23 +1,43 @@
-# Interview Scheduler
+# Evolve
 
-## Setup
+A game that allows users to build their own colony of 'Orgs'. They can add orgs, feed their colony, place barriers around their creatures and watch their orgs evolve. 
+Front-end: HTML, Sass, React
+Back-end: Rails, PSQL
+Testing: Cypress, Rspec
 
-Install dependencies with `npm install`.
+## Final Product
 
-## Running Webpack Development Server
+![gif]()
 
-```sh
-npm start
-```
+!["Initial View"]()
+!["Register"]()
+!["Login"]()
+!["new-game"]()
+!["Game1"]()
+!["Game2"]()
+!["Game3"]()
+!["Game4"]()
+!["Game5"]()
 
-## Running Jest Test Framework
 
-```sh
-npm test
-```
+## Deployed version Interview Scheduler using netlify and heroku:
+[Evolve](https://festive-ritchie-f7db49.netlify.com/)
 
-## Running Storybook Visual Testbed
+## Dependencies
 
-```sh
-npm run storybook
-```
+- Axios
+- Classnames
+- Phaser 3
+- Ion-phaser
+- Gaussian
+- React
+- React-dom
+- React-scripts
+- React Joyride
+- React Cookie
+
+## Getting Started
+
+- Install all dependencies (using the `npm install` command).
+- Run the development web server using the `npm start` command.
+
--- src/components/PageContent/Guide.js
+++ src/components/PageContent/Guide.js
@@ -6,19 +6,20 @@ export default function Guide(props) {
   let user_id = props.cookies.user_id
   return (
     <section>
-       <h2  className="guide-welcome-text">Welcome to Evolve where you can build your very own colony of orgs! They will evolve before your very eyes!</h2>
+       <h2  className="guide-welcome-text">Welcome to Evolve where you can build your very own colony of Orgs! They will evolve before your very eyes!</h2>
       <ul className="guide-text">
-        <li>To start a new game, go to Game in the nav-bar. Alternatively, once you're made an account you can load a saved game to its last saved state.</li>
-        <li>Clicking Add Org lets you add more orgs into your colony</li>
-        <li>Clicking Add food positions food randomly on the org-world </li>
-        <li>Got a favourite org you want to feed? Toggling feed lets you add food whereever you click.</li>
-        <li>The add blocks toggle let you build walls on the org ground to save your orgs from predators or stop a greedy org from eating all the food!</li>
-        <li>The delete toggle lets you delete any unwanted food, orgs and blocks by clicking on them</li>
-        <li>Once you've made an account you can click on save to store your game, clicking load on the home-game page lets you continue any saved game.</li>
-        <li>Toggling predator mode lets you enable to naturally predatory orgs to attack! </li>
-        <li>Clicking on an org lets you see all of his or her attributes</li>
-        <li>At the top of the screen you can see the main statistics for your colony.</li>
-        <li>You can hide the menu buttons by clicking on the menu icon in the top left corner</li>
+        <li>To start a new game, go to Game in the nav-bar. Alternatively, once you're made an account, you can load a saved game from its last saved state.</li>
+        <li>Clicking 'Add Org' lets you add more Orgs into your colony.</li>
+        <li>Clicking 'Add food' positions food randomly on the Org-world. </li>
+        <li>Got a favourite Org you want to feed? Toggling feed lets you add food wherever you click.</li>
+        <li>When you start the game, you're in 'draggable' mode, this lets you drag Orgs, food and blocks anywhere you please.</li>
+        <li>The 'add blocks' toggle lets you build walls in the Org-world to save your Orgs from predators or stop a greedy Org from eating all the food!</li>
+        <li>The 'delete' toggle lets you delete any unwanted food, Orgs and blocks simply by clicking on them.</li>
+        <li>Once you've made an account, you can click on save to store your game, clicking load on the main-games page lets you continue any saved game.</li>
+        <li>Toggling predator mode lets you enable to naturally predatory Orgs to attack! </li>
+        <li>Clicking on an Org lets you see all of his or her attributes.</li>
+        <li>At the top of the screen you can see the key statistics for your colony.</li>
+        <li>You can hide the menu buttons by clicking on the menu icon in the top left corner.</li>
         <li>You can make the game full screen by clicking the icon in the top right. You can also turn the music on or off with the speaker button.</li>
       </ul>
       <h2 className="guide-welcome-text">Happy evolving!</h2>
